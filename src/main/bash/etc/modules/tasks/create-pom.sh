a391d5f2d65a9a954be740f7d207fff61ff02050
vdmeer <sven@vandermeer.de>
2015-07-30 22:05:34 +0100

    added fancy output

--- src/main/bash/etc/modules/tasks/create-pom.sh
+++ src/main/bash/etc/modules/tasks/create-pom.sh
@@ -30,24 +30,30 @@
 ##
 
 if [ -z "$MVN_GROUP_ID" ]; then
-	echo "no groupId set, use \$MVN_GROUP_ID"
+	echo ""
+	echo " ---> no groupId set, use \$MVN_GROUP_ID"
 	exit 255
 fi
 
 source $MOD_FILE_VERSION_BASH
 
+echo -n "."
 file_extension=".pom.xml"
 gen_directory=$MOD_TARGET_MODULES_DIR/generated-poms
 if [ ! -d $gen_directory ]; then
 	mkdir $gen_directory
 fi
 
+echo -n "."
 out_fn_fqpn=$gen_directory/$skb_module_artifact$file_extension
 echo -n "" > $out_fn_fqpn
 
 pom_fn_fqpn=$skb_module_directory/pom.xml
 
+echo -n "."
 cat $MOD_ETC_POMART_DIR/project-open.skb >> $out_fn_fqpn
+
+	echo -n "."
 	cat $MOD_ETC_POMART_DIR/modelVersion.skb >> $out_fn_fqpn
 	echo "	<groupId>$MVN_GROUP_ID</groupId>" >> $out_fn_fqpn
 	echo "	<artifactId>$skb_module_artifact</artifactId>" >> $out_fn_fqpn
@@ -59,12 +65,14 @@ cat $MOD_ETC_POMART_DIR/project-open.skb >> $out_fn_fqpn
 	fi
 	echo "" >> $out_fn_fqpn
 
+	echo -n "."
 	echo "	<name>$skb_module_name</name>" >> $out_fn_fqpn
 	echo "	<url>$skb_module_url</url>" >> $out_fn_fqpn
 	echo "	<description>$skb_module_description</description>" >> $out_fn_fqpn
 	echo "	<inceptionYear>$skb_module_inceptionYear</inceptionYear>" >> $out_fn_fqpn
 	echo "" >> $out_fn_fqpn
 
+	echo -n "."
 	echo "	<properties>" >> $out_fn_fqpn
 		echo "		<maven.compiler.source>$skb_module_properties_compiler_source</maven.compiler.source>" >> $out_fn_fqpn
 		echo "		<maven.compiler.target>$skb_module_properties_compiler_target</maven.compiler.target>" >> $out_fn_fqpn
@@ -76,15 +84,19 @@ cat $MOD_ETC_POMART_DIR/project-open.skb >> $out_fn_fqpn
 	echo "	</properties>" >> $out_fn_fqpn
 	echo "" >> $out_fn_fqpn
 
+	echo -n "."
 	cat $MOD_ETC_POMART_DIR/licence.skb >> $out_fn_fqpn
 	echo "" >> $out_fn_fqpn
 
+	echo -n "."
 	cat $MOD_ETC_POMART_DIR/prerequisites.skb >> $out_fn_fqpn
 	echo "" >> $out_fn_fqpn
 
+	echo -n "."
 	cat $MOD_ETC_POMART_DIR/distributionManagement.skb >> $out_fn_fqpn
 	echo "" >> $out_fn_fqpn
 
+	echo -n "."
 	echo "	<scm>" >> $out_fn_fqpn
 		echo "		<developerConnection>$skb_module_scm_developerConnection</developerConnection>" >> $out_fn_fqpn
 		echo "		<connection>$skb_module_scm_connection</connection>" >> $out_fn_fqpn
@@ -93,12 +105,14 @@ cat $MOD_ETC_POMART_DIR/project-open.skb >> $out_fn_fqpn
 	echo "	</scm>" >> $out_fn_fqpn
 	echo "" >> $out_fn_fqpn
 
+	echo -n "."
 	echo "	<issueManagement>" >> $out_fn_fqpn
 		echo "		<url>$skb_module_issueManagement_url</url>" >> $out_fn_fqpn
 		echo "		<system>$skb_module_issueManagement_system</system>" >> $out_fn_fqpn
 	echo "	</issueManagement>" >> $out_fn_fqpn
 	echo "" >> $out_fn_fqpn
 
+	echo -n "."
 	echo "	<developers>" >> $out_fn_fqpn
 		if [ -f $skb_module_directory/$MOD_MODULE_SETTINGS_DIR/developers.skb ] ; then
 			cat $skb_module_directory/$MOD_MODULE_SETTINGS_DIR/developers.skb >> $out_fn_fqpn
@@ -106,6 +120,7 @@ cat $MOD_ETC_POMART_DIR/project-open.skb >> $out_fn_fqpn
 	echo "	</developers>" >> $out_fn_fqpn
 	echo "" >> $out_fn_fqpn
 
+	echo -n "."
 	echo "	<contributors>" >> $out_fn_fqpn
 		if [ -f $skb_module_directory/$MOD_MODULE_SETTINGS_DIR/contributors.skb ] ; then
 			cat $skb_module_directory/$MOD_MODULE_SETTINGS_DIR/contributors.skb >> $out_fn_fqpn
@@ -113,6 +128,7 @@ cat $MOD_ETC_POMART_DIR/project-open.skb >> $out_fn_fqpn
 	echo "	</contributors>" >> $out_fn_fqpn
 	echo "" >> $out_fn_fqpn
 
+	echo -n "."
 	echo "	<dependencies>" >> $out_fn_fqpn
 		for intdep in $skb_module_internalDependencies
 		do
@@ -129,6 +145,7 @@ cat $MOD_ETC_POMART_DIR/project-open.skb >> $out_fn_fqpn
 	echo "	</dependencies>" >> $out_fn_fqpn
 	echo "" >> $out_fn_fqpn
 
+	echo -n "."
 	echo "	<build>" >> $out_fn_fqpn
 	echo "		<plugins>" >> $out_fn_fqpn
 	if [ -f $skb_module_directory/$MOD_MODULE_SETTINGS_DIR/plugins.skb ] ; then
@@ -137,28 +154,34 @@ cat $MOD_ETC_POMART_DIR/project-open.skb >> $out_fn_fqpn
 	cat $MOD_ETC_POMART_DIR/plugin-jar.skb >> $out_fn_fqpn
 	cat $MOD_ETC_POMART_DIR/plugin-compiler.skb >> $out_fn_fqpn
 
+	echo -n "."
 	if [ -f $skb_module_directory/$MOD_MODULE_SETTINGS_DIR/plugin-javadoc.skb ] ; then
 		cat $skb_module_directory/$MOD_MODULE_SETTINGS_DIR/plugin-javadoc.skb >> $out_fn_fqpn
 	else
 		cat $MOD_ETC_POMART_DIR/plugin-javadoc.skb >> $out_fn_fqpn
 	fi
 
-	#cat $MOD_ETC_POMART_DIR/plugin-dependency.skb >> $out_fn_fqpn
+	echo -n "."
 	cat $MOD_ETC_POMART_DIR/plugin-sourcejar.skb >> $out_fn_fqpn
 	echo "		</plugins>" >> $out_fn_fqpn
 	echo "	</build>" >> $out_fn_fqpn
 	echo "" >> $out_fn_fqpn
 
+	echo -n "."
 	if [ -f $skb_module_directory/$MOD_MODULE_SETTINGS_DIR/modules.skb ] ; then
 		cat $skb_module_directory/$MOD_MODULE_SETTINGS_DIR/modules.skb >> $out_fn_fqpn
 		echo "" >> $out_fn_fqpn
 	fi
 
+	echo -n "."
 	if [ -f $skb_module_directory/$MOD_MODULE_SETTINGS_DIR/profiles.skb ] ; then
 		cat $skb_module_directory/$MOD_MODULE_SETTINGS_DIR/profiles.skb >> $out_fn_fqpn
 		echo "" >> $out_fn_fqpn
 	fi
 
+echo -n "."
 cat $MOD_ETC_POMART_DIR/project-close.skb >> $out_fn_fqpn
 
+echo -n "."
 cat $out_fn_fqpn > $pom_fn_fqpn
+echo -n " -> done"
