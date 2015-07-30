8c39de429a96de030b6d23aa7188ad23cd4cc0cf
vdmeer <sven@vandermeer.de>
2015-07-30 22:40:46 +0100

    added settings tests, removed folder tests

--- src/main/bash/etc/modules/tasks/test-files.sh
+++ src/main/bash/etc/modules/tasks/test-files.sh
@@ -28,17 +28,103 @@
 ## call with sourced settings file
 ##
 
-if [ ! -d $MOD_TARGET_MODULES_DIR ]; then
-	echo "no target directory found, cannot proceed, try option -i"
-	exit 255
-fi
-if [ ! -f $MOD_FILE_VERSION_BASH ]; then
-	echo "no version file for BASH found, cannot proceed, try option -i"
-	exit 255
-fi
-if [ ! -f $MOD_TARGET_MODULES_DIR/$skb_module_artifact.bash ]; then
-	echo "no BASH settings file for module <$skb_module_artifact> found, cannot proceed, try option -i for module <$skb_module_artifact>"
-	exit 255
+echo -n "."
+fn_settings=$skb_module_directory/$MOD_MODULE_SETTINGS_DIR/$skb_module_artifact.settings
+
+echo -n "."
+if [ -z "`cat $fn_settings | grep skb.module.description`" ]; then
+	echo ""
+	echo "---> settings missing argument ==> skb.module.description"
 fi
 
-echo "------> all files ok for module <$skb_module_artifact>"
+echo -n "."
+if [ -z "`cat $fn_settings | grep skb.module.internalDependencies`" ]; then
+	echo ""
+	echo "---> settings missing argument ==> skb.module.internalDependencies"
+fi
+
+echo -n "."
+if [ -z "`cat $fn_settings | grep skb.module.version`" ]; then
+	echo ""
+	echo "---> settings missing argument ==> skb.module.version"
+fi
+
+echo -n "."
+if [ -z "`cat $fn_settings | grep skb.module.alias`" ]; then
+	echo ""
+	echo "---> settings missing argument ==> skb.module.alias"
+fi
+
+echo -n "."
+if [ -z "`cat $fn_settings | grep skb.module.name`" ]; then
+	echo ""
+	echo "---> settings missing argument ==> skb.module.name"
+fi
+
+echo -n "."
+if [ -z "`cat $fn_settings | grep skb.module.url`" ]; then
+	echo ""
+	echo "---> settings missing argument ==> skb.module.url"
+fi
+
+echo -n "."
+if [ -z "`cat $fn_settings | grep skb.module.inceptionYear`" ]; then
+	echo ""
+	echo "---> settings missing argument ==> skb.module.inceptionYear"
+fi
+
+echo -n "."
+if [ -z "`cat $fn_settings | grep skb.module.packaging`" ]; then
+	echo ""
+	echo "---> settings missing argument ==> skb.module.packaging"
+fi
+
+echo -n "."
+if [ -z "`cat $fn_settings | grep skb.module.properties.compiler.source`" ]; then
+	echo ""
+	echo "---> settings missing argument ==> skb.module.properties.compiler.source"
+fi
+
+echo -n "."
+if [ -z "`cat $fn_settings | grep skb.module.properties.compiler.target`" ]; then
+	echo ""
+	echo "---> settings missing argument ==> skb.module.properties.compiler.target"
+fi
+
+echo -n "."
+if [ -z "`cat $fn_settings | grep skb.module.properties.encoding`" ]; then
+	echo ""
+	echo "---> settings missing argument ==> skb.module.properties.encoding"
+fi
+
+echo -n "."
+if [ -z "`cat $fn_settings | grep skb.module.scm.developerConnection`" ]; then
+	echo ""
+	echo "---> settings missing argument ==> skb.module.scm.developerConnection"
+fi
+
+echo -n "."
+if [ -z "`cat $fn_settings | grep skb.module.scm.connection`" ]; then
+	echo ""
+	echo "---> settings missing argument ==> skb.module.scm.connection"
+fi
+
+echo -n "."
+if [ -z "`cat $fn_settings | grep skb.module.scm.url`" ]; then
+	echo ""
+	echo "---> settings missing argument ==> skb.module.scm.url"
+fi
+
+echo -n "."
+if [ -z "`cat $fn_settings | grep skb.module.issueManagement.url`" ]; then
+	echo ""
+	echo "---> settings missing argument ==> skb.module.issueManagement.url"
+fi
+
+echo -n "."
+if [ -z "`cat $fn_settings | grep skb.module.issueManagement.system`" ]; then
+	echo ""
+	echo "---> settings missing argument ==> skb.module.issueManagement.system"
+fi
+
+echo -n "-> settings ok "
