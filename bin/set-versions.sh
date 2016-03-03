c9361fa0e0ac571746c3d1bdb604c8ab72574bb5
vdmeer <sven@vandermeer.de>
2016-03-03 11:52:11 +0000

    added comments

--- bin/set-versions.sh
+++ bin/set-versions.sh
@@ -1,17 +1,87 @@
 #!/bin/bash
 
-system=`uname -o`
-home_abs=`pwd`
+## Copyright 2016 Sven van der Meer <vdmeer.sven@mykolab.com>
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+##
 
-if [ "$system" == "Cygwin" ] ; then
-	if [[ $home_abs == *"/cygdrive"* ]]; then
-		home_sh="/"`echo $home_abs | cut -d/ -f4-`
+##
+## Sets file versions for a particular project using given version properties.
+## Call
+##
+## @package    de.vandermeer.skb
+## @author     Sven van der Meer <vdmeer.sven@mykolab.com>
+## @copyright  2014-2015 Sven van der Meer
+## @license    http://www.apache.org/licenses/LICENSE-2.0  Apache License, Version 2.0
+## @version    v2.1.0 build 160226 (26-Feb-16)
+
+
+##
+## Get system, need this information for creating build scripts
+## taken from: https://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linux#3466183 - see there for details
+##
+GetSystem()
+{
+	case "$(uname -s)" in
+		Darwin)
+			echo 'found Mac OS X'
+			system=MAC
+			;;
+		Linux)
+			echo 'Linux'
+			system=LINUX
+			;;
+		CYGWIN*)
+			echo 'found Cygwin'
+			system=CYGWIN
+			;;
+		MINGW32*|MSYS*)
+			echo 'found MS Windows'
+			system=WINDOWS
+			;;
+		*)
+			echo 'found other OS'
+			system="???" 
+			;;
+	esac
+}
+
+
+##
+## Sets directories for specific systems (call GetSystem first)
+##
+SetDirectories()
+{
+	home_abs=`pwd`
+
+	CP=${LIB_HOME}/*
+	SCRIPT_NAME=`basename $0`
+
+	echo "set directories: $system"
+	if [ "$system" == "CYGWIN" ] ; then
+		if [[ $home_abs == *"/cygdrive"* ]]; then
+			home_sh="/"`echo $home_abs | cut -d/ -f4-`
+		else
+			home_sh=${home_abs}
+		fi
 	else
 		home_sh=${home_abs}
 	fi
-else
-	home_sh=${home_abs}
-fi
+}
+
+
+GetSystem
+SetDirectories
 
 ant="ant -f src/bundle/pm/set-versions/build.xml -DversionFile=$home_sh/src/bundle/pm/set-versions/versions.properties"
 
@@ -21,26 +91,3 @@ do
 	prj_dir=`echo $dir | sed -e 's/^.*=//'`
 	$ant -DmoduleDir=$home_sh/$prj_dir -DmoduleFile=$home_sh/$prj_dir/src/bundle/pm/project.properties
 done
-
-#ant -f src/bundle/pm/set-versions/build.xml -DmoduleFile=$home_sh/../mvn/project-manager/src/bundle/pm/project.properties -DmoduleDir=$home_sh/../mvn/project-manager
-#
-#-DmoduleFile=$home_sh/../asciiparagraph/src/bundle/pm/project.properties
-#-DmoduleDir=$home_sh/../asciiparagraph
-#
-#-DmoduleFile=$home_sh/../asciitable/src/bundle/pm/project.properties
-#-DmoduleDir=$home_sh/../asciitable
-#
-#-DmoduleFile=$home_sh/../asciilist/src/bundle/pm/project.properties
-#-DmoduleDir=$home_sh/../asciilist
-#
-#-DmoduleFile=$home_sh/../execs/src/bundle/pm/project.properties
-#-DmoduleDir=$home_sh/../execs
-#
-#-DmoduleFile=$home_sh/../base/src/bundle/pm/project.properties
-#-DmoduleDir=$home_sh/../base
-#
-#-DmoduleFile=$home_sh/../datatool/src/bundle/pm/project.properties
-#-DmoduleDir=$home_sh/../datatool
-#
-#-DmoduleFile=$home_sh/../examples/src/bundle/pm/project.properties
-#-DmoduleDir=$home_sh/../examples
